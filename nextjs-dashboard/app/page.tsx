2115bbef5c35c7896cdcf2b105167ca155e7eb26
Luis Fernando Moreno Cabrera <luxurux@MacBook-Air-de-Luis-2.local>
2025-05-29 18:07:29 -0600

    Fix redirection from root to dashboard

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,15 +1,9 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
-import { lusitana } from '@/app/ui/fonts';
-import Image from 'next/image'
 // app/page.tsx
 
-// app/page.tsx
 import { redirect } from 'next/navigation';
 
-export default function Page() {
-  redirect('/dashboard'); // No debes retornar JSX si rediriges
+export default function Home() {
+  redirect('/dashboard');
+  return null;
 }
 
